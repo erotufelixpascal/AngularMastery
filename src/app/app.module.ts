bf71ae9cf94e482da351e9305f62ccf0abadd96c
Felix Erotu <erotufelixpascal2@gmail.com>
2023-11-14 11:38:41 +0300

    NewFeature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule} from '@angular/material/core'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,9 @@ import {MatNativeDateModule} from '@angular/material/core'
     MatSelectModule,
     MatRadioModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { FormGroup , FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-dialog',
@@ -6,13 +7,23 @@ import { Component,OnInit } from '@angular/core';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
-  freshnessList = ["Brandnew", "Refurbished", "Second Hand"]
+  freshnessList = ["Brandnew", "Refurbished", "Second Hand"];
+  productForm !: FormGroup;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
   
   ngOnInit(): void {
       //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
       //Add 'implements OnInit' to the class.
+      this.productForm = this .formBuilder.group({
+        productName : ['',Validators.required],
+        category :['',Validators.required],
+        freshness :['',Validators.required],
+        price : ['',Validators.required],
+        comment : ['',Validators.required],
+        date : ['',Validators.required]
+
+      })
       
     }
 
