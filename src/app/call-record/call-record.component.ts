dd1dbcc02ac687023abcedddc0a0dcae6c8da709
Felix <erotufelixpascal2@gmail.com>
2023-12-08 13:58:20 +0300

    newfeature

--- src/app/call-record/call-record.component.ts
+++ src/app/call-record/call-record.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class CallRecordComponent {
   data =[
     {Name:"Stefan", Email:'stefan.heinz@gmail.com', Phone:'+1-46363-6464', SkypeID:'',WhatsAppID:'', Timezone:'MountainTime',Country:'United States',BestTime:'Various',Preferedmethod:'email',Forum:'Yes,07/1997,Munich,David Norris',Listener:'Stelan Heinz'},
-    {Name:"Stefan", Email:'stefan.heinz@gmail.com', Phone:'+1-46363-6464', SkypeID:'',WhatsAppID:'', Timezone:'',Country:'',BestTimes:'',Preferedmethod:'',Forum:'',Listener:''}
+    {Name:"FELIX", Email:'felix@gmail.com', Phone:'+256-786-4647', SkypeID:'',WhatsAppID:'', Timezone:'',Country:'',BestTimes:'',Preferedmethod:'',Forum:'',Listener:''}
   ]
 
 }
