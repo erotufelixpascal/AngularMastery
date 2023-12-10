e50f9cf57828f809ae551863ed06e74014ae40b6
Felix <erotufelixpascal2@gmail.com>
2023-12-10 05:18:53 +0300

    newfeature

--- src/app/weekend-report/weekend-report.component.ts
+++ src/app/weekend-report/weekend-report.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-weekend-report',
   templateUrl: './weekend-report.component.html',
   styleUrls: ['./weekend-report.component.scss']
 })
-export class WeekendReportComponent {
+export class WeekendReportComponent implements OnInit {
+  weekendReportForm!:FormGroup;
+
+  constructor(private fb1:FormBuilder){}
+
+  ngOnInit()  {
+    this.weekendReportForm = this.fb1.group({
+      weekendDate:'',
+      questionOne: [''],
+      questionTwo: [''],
+      questionThree: [''],
+      questionFour: [''],
+      questionFive: [''],
+      questionSix: [''],
+          
+    })
+    
+  }
+
+  onFormSubmit(event: Event){
+    // if(this.weekendReportForm.valid){
+    //   event.preventDefault();
+    //   console.log(this.weekendReportForm.value);
+    // }
+    event.preventDefault();
+      console.log(this.weekendReportForm.value);
+  }
+    
+  
 
 }
