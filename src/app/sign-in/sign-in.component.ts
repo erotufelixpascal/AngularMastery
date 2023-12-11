14093ab1db36887c314776d98cc90e6abdf1d703
Felix <erotufelixpascal2@gmail.com>
2023-12-11 10:47:09 +0300

    newfeature

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss']
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit {
+  signInForm! : FormGroup;
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit() {
+    this.signInForm = this.fb.group({
+      'fname':['',Validators.required],
+      'lname':['',Validators.required],
+      'email':['',Validators.email],
+      'telephone':[''],
+      'username':['',Validators.required],
+      'password':['',Validators.required],
+
+  
+    })
+    
+  }
+  SignIn(event:Event){
+    event.preventDefault();
+    console.log(this.signInForm.value)
+
+  }
+
+
+  
 
 }
