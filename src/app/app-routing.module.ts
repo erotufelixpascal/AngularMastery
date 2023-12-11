203ca58771bf0d38ecdeed36ed4fcadb2a6bc129
Felix <erotufelixpascal2@gmail.com>
2023-12-11 10:58:31 +0300

    newfeature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { WeekendReportComponent } from './weekend-report/weekend-report.component';
+import { CallRecordComponent } from './call-record/call-record.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent},
+  {path:'login', component:LoginComponent},
+  {path:'signIn', component:SignInComponent},
+  {path:'weekendReport', component:WeekendReportComponent},
+  {path:'callRecord', component:CallRecordComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
