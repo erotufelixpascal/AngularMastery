c34e89ceccca43ad05d0e4dbe644a37bd8590517
Felix <erotufelixpascal2@gmail.com>
2023-12-19 11:57:57 +0300

    newfeature

--- src/app/manage-relationships/manage-relationships.component.ts
+++ src/app/manage-relationships/manage-relationships.component.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage-relationships.component.scss']
 })
 export class ManageRelationshipsComponent {
-  Relationships =["Richard", "Jane" , "Peace" , "Tom" , "Foster", "Felix"];
+  
+  Relationships =[
+    {name:"Richard",Relationshipfor:"Landmark",Active:"Yes",order:"1"},
+    {name:"Jane",Relationshipfor:"Home",Active:"No",order:"2"},
+    {name:"Peace",Relationshipfor:"School",Active:"Yes",order:"3"},
+    {name:"Emily",Relationshipfor:"Neighbourhood",Active:"No",order:"4"},
+    {name:"Foster",Relationshipfor:"Gym",Active:"Yes",order:"5"},
+    {name:"Felix",Relationshipfor:"Work",Active:"No",order:"6"},
+    {name:"Kris",Relationshipfor:"Landmark",Active:"Yes",order:"7"},
+  ]
 
 }
