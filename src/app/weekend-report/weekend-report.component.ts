bb889d3aa3e59c277105a8367041dbf611de0d7c
Felix <erotufelixpascal2@gmail.com>
2023-12-21 09:32:31 +0300

    newfeature

--- src/app/weekend-report/weekend-report.component.ts
+++ src/app/weekend-report/weekend-report.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormBuilder } from '@angular/forms';
-import { DialogService } from '../services/dialog.service';
-import { DialogRef } from '@angular/cdk/dialog';
+// import { DialogService } from '../services/dialog.service';
+// import { DialogRef } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-weekend-report',
@@ -13,8 +13,9 @@ export class WeekendReportComponent implements OnInit {
 
   constructor(
     private fb1:FormBuilder,
-    private _callService:DialogService, 
-    private _callRef: DialogRef<WeekendReportComponent>){}
+    // private _callService:DialogService, 
+    // private _callRef: DialogRef<WeekendReportComponent>
+    ){}
 
   ngOnInit()  {
     this.weekendReportForm = this.fb1.group({
@@ -31,12 +32,12 @@ export class WeekendReportComponent implements OnInit {
   }
 
   onFormSubmit(event: Event){
-    // if(this.weekendReportForm.valid){
-    //   event.preventDefault();
-    //   console.log(this.weekendReportForm.value);
-    // }
-    event.preventDefault();
+    if(this.weekendReportForm.valid){
+      event.preventDefault();
       console.log(this.weekendReportForm.value);
+    }
+    // event.preventDefault();
+    //   console.log(this.weekendReportForm.value);
   }
     
   
