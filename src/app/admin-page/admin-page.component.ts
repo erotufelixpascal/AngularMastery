5ff6ae7ef21cb0abd91daafd7c092fce4c799645
Felix <erotufelixpascal2@gmail.com>
2023-12-28 02:08:06 +0300

    newfeature

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { DataService } from 'data.service';
+import { DataServiceService } from '../data-service.service';
+
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.scss']
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit {
   users=["Felix P"];
+
+  constructor (private dataService:DataServiceService){};
+
+  ngOnInit() {
+    // Example: Fetch data
+    this.dataService.getData().subscribe((data) => {
+      console.log(data);
+    });
+
+    // Example: Post data
+    const postData = { key: 'value' };
+    this.dataService.postData(postData).subscribe((response) => {
+      console.log(response);
+    });
+    
+  }
 
 }
