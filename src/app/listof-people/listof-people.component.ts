668a2ae97f3897229ee90d02c8666e281b541e74
Felix <erotufelixpascal2@gmail.com>
2024-01-02 12:50:39 +0300

    newfeature

--- src/app/listof-people/listof-people.component.ts
+++ src/app/listof-people/listof-people.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./listof-people.component.scss']
 })
 export class ListofPeopleComponent {
+  People= [
+    {id:"1",firstName:"Jane",lastName:"Jory",Contact:"+1-234-434-984",Role:"Participant",AssignedCoursePartner:"",Callscheduled:"Yes",Status:"Active"},
+    {id:"2",firstName:"Felix",lastName:"Micheal",Contact:"+77-234-434",Role:"",AssignedCoursePartner:"",Callscheduled:"Yes",Status:"Not Active"},
+  
+  ]
 
 }
